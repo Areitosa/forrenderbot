7b011f9024d143ad40dd680cf8b841f28caa30fa
Areitosa <120257212+Areitosa@users.noreply.github.com>
2023-01-03 22:24:41 -0500

    Update function.py

--- function.py
+++ function.py
@@ -2,7 +2,7 @@ import telegram
 from flask import request
 
 def handler(request):
-    bot = telegram.Bot(token=TOKEN)
+    bot = telegram.Bot(token=5464846915:AAE1qlyt5AkJOGsNWJe6x_a9aRqS0QscSwo)
 
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)

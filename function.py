9f7096e8fa99b22fe1584694571ce824bc884f16
Areitosa <120257212+Areitosa@users.noreply.github.com>
2023-01-03 22:33:03 -0500

    Update function.py

--- function.py
+++ function.py
@@ -2,12 +2,16 @@ import telegram
 from flask import request
 
 def handler(request):
-    bot = telegram.Bot(token=5464846915:AAE1qlyt5AkJOGsNWJe6x_a9aRqS0QscSwo)
+    try:
+        bot = telegram.Bot(token='5464846915:AAE1qlyt5AkJOGsNWJe6x_a9aRqS0QscSwo')
 
-    if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-        chat_id = update.message.chat.id
-        message = update.message.text
-        bot.send_message(chat_id=chat_id, text="You said: {}".format(message))
+        if request.method == "POST":
+            update = telegram.Update.de_json(request.get_json(force=True), bot)
+            chat_id = update.message.chat.id
+            message = update.message.text
+            bot.send_message(chat_id=chat_id, text="You said: {}".format(message))
+
+    except Exception as e:
+        print(e)
 
     return "ok"
